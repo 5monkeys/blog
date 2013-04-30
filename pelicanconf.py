3f5c377af9d24f33aa5c4f7ad11597e462acf0f0
Jonas Lundberg <jonas@5monkeys.se>
2013-04-30 14:48:34 +0200

    Add pelican

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Monkey'
+SITENAME = u'5 Monkeys'
+SITESUBTITLE = u'labs blog'
+SITEURL = 'http://5monkeys.github.io/blog/'
+
+TIMEZONE = 'Europe/Stockholm'
+DEFAULT_LANG = u'en'
+
+THEME = 'themes/monkey'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None  #'feeds/all.atom.xml'
+FEED_ALL_RSS = None  #'feeds/all.rss'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+TYPOGRIFY = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+
+LINKS = []
+SOCIAL = []
+DEFAULT_PAGINATION = False
+RELATIVE_URLS = True
